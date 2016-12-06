868223797c544d06a03feee9be6dafa82c35a52c
Matthew Hartley <Matthew.Hartley@jic.ac.uk>
2016-12-06 11:05:53 +0000

    Add generate_full_file list with test

--- dtool.py
+++ dtool.py
@@ -1,3 +1,31 @@
 #!/usr/bin/env python2
 """Tool for managing JIC archive data."""
 
+import os
+
+def generate_manifest(path):
+    """Return archive data structure.
+
+    Structure includes all files in the filesystem rooted at path with:
+
+    * Relative path
+    * SHA1 hash
+    * Last modification time
+    * Size
+
+    """
+
+def generate_full_file_list(path):
+    """Return a list of fully qualified paths to all files in directories under
+    the given path."""
+
+    file_list = []
+
+    path_length = 1 + len(path)
+    
+    for dirpath, dirnames, filenames in os.walk(path):
+        for fn in filenames:
+            relative_path = os.path.join(dirpath, fn)
+            file_list.append(relative_path[path_length:])
+
+    return file_list
--- tests.py
+++ tests.py
@@ -6,6 +6,17 @@ import json
 TEST_INPUT_DATA = 'test_data/input'
 TEST_OUTPUT_DATA = 'test_data/output'
 
+def test_generate_full_file_list():
+
+    from dtool import generate_full_file_list
+
+    expected = ['file1.txt', 'dir1/file2.txt']
+
+    test_archive_path = os.path.join(TEST_INPUT_DATA, 'archive')
+    actual = generate_full_file_list(test_archive_path)
+
+    assert sorted(actual) == sorted(expected), actual
+
 def test_generate_manifest():
 
     from dtool import generate_manifest
@@ -17,4 +28,4 @@ def test_generate_manifest():
     test_archive_path = os.path.join(TEST_INPUT_DATA, 'archive')
     actual = generate_manifest(test_archive_path)
 
-    actual == expected
+    assert(actual == expected)
