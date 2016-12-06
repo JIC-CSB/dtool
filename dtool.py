246343e452a72d6c08978a9968e94dd4abe71c11
Matthew Hartley <Matthew.Hartley@jic.ac.uk>
2016-12-06 11:50:44 +0000

    Complete generation of manifests with passing tests

--- dtool.py
+++ dtool.py
@@ -2,9 +2,24 @@
 """Tool for managing JIC archive data."""
 
 import os
+import hashlib
+import argparse
+
+def shasum(filename):
+    """Return hex digest of SHA-1 hash of file."""
+
+    BUF_SIZE = 65536
+    hasher = hashlib.sha1()
+    with open(filename, 'rb') as f:
+        buf = f.read(BUF_SIZE)
+        while len(buf) > 0:
+            hasher.update(buf)
+            buf = f.read(BUF_SIZE)
+
+    return hasher.hexdigest()
 
 def generate_manifest(path):
-    """Return archive data structure.
+    """Return manfifest data structure.
 
     Structure includes all files in the filesystem rooted at path with:
 
@@ -14,6 +29,40 @@ def generate_manifest(path):
     * Size
 
     """
+
+    full_file_list = generate_full_file_list(path)
+
+    entries = []
+    for filename in full_file_list:
+        fq_filename = os.path.join(path, filename)
+        st_size = os.stat(fq_filename).st_size
+        st_mtime = os.stat(fq_filename).st_mtime
+        file_hash = shasum(fq_filename)
+
+        entry = {}
+        entry['path'] = filename
+        entry['hash'] = file_hash
+        entry['size'] = st_size
+        entry['mtime'] = st_mtime
+        entries.append(entry)
+
+    return entries
+
+def generate_archive_manifest(path):
+    """Return archive data structure.
+
+    This is a file manifest (see generate_manifest) rooted at the 'archive'
+    level.
+    """
+
+    raw_manifest = generate_manifest(path)
+
+    for entry in raw_manifest:
+        filename = entry['path']
+        archive_filename = os.path.join('archive', filename)
+        entry['path'] = archive_filename
+
+    return raw_manifest 
 
 def generate_full_file_list(path):
     """Return a list of fully qualified paths to all files in directories under
@@ -29,3 +78,16 @@ def generate_full_file_list(path):
             file_list.append(relative_path[path_length:])
 
     return file_list
+
+def create_archive_manifest():
+
+    pass
+
+def main():
+
+    parser = argparse.ArgumentParser(__doc__)  
+
+    
+
+if __name__ == "__main__":
+    main()
--- tests.py
+++ tests.py
@@ -6,6 +6,30 @@ import json
 TEST_INPUT_DATA = 'test_data/input'
 TEST_OUTPUT_DATA = 'test_data/output'
 
+def test_shasum():
+
+    from dtool import shasum
+
+    expected = "a250369afb3eeaa96fb0df99e7755ba784dfd69c"
+
+    test_file = os.path.join(TEST_INPUT_DATA, 'archive', 'file1.txt')
+    actual = shasum(test_file)
+
+    assert actual == expected
+
+def test_generate_manifest():
+
+    from dtool import generate_manifest
+
+    expected_manifest_file = os.path.join(TEST_OUTPUT_DATA, 'raw_manifest.json')
+    with open(expected_manifest_file) as fh:
+        expected = json.load(fh)
+
+    test_archive_path = os.path.join(TEST_INPUT_DATA, 'archive')
+    actual = generate_manifest(test_archive_path)
+
+    assert(actual == expected)
+
 def test_generate_full_file_list():
 
     from dtool import generate_full_file_list
@@ -15,17 +39,17 @@ def test_generate_full_file_list():
     test_archive_path = os.path.join(TEST_INPUT_DATA, 'archive')
     actual = generate_full_file_list(test_archive_path)
 
-    assert sorted(actual) == sorted(expected), actual
+    assert sorted(actual) == sorted(expected)
 
-def test_generate_manifest():
+def test_generate_archive_manifest():
 
-    from dtool import generate_manifest
+    from dtool import generate_archive_manifest
 
     expected_manifest_file = os.path.join(TEST_OUTPUT_DATA, 'manifest.json')
     with open(expected_manifest_file) as fh:
         expected = json.load(fh)
 
     test_archive_path = os.path.join(TEST_INPUT_DATA, 'archive')
-    actual = generate_manifest(test_archive_path)
+    actual = generate_archive_manifest(test_archive_path)
 
-    assert(actual == expected)
+    assert actual == expected
