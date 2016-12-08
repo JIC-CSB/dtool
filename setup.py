4e63e8b78ec6bf331d0290f31c5c754d7fe9d5c3
Tjelvar Olsson <tjelvar.olsson@jic.ac.uk>
2016-12-08 00:46:10 +0000

    Add more info to setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,14 @@ from setuptools import setup
 
 
 version = "0.2.0"
+readme = open("README.rst").read()
+url = "https://github.com/JIC-CSB/dtool"
 
 setup(name="dtool",
+      packages=["dtool"],
       version=version,
-      packages=["dtool"])
+      description="Tools for managing scientific data",
+      long_description=readme,
+      url=url,
+      download_url="{}/tarball/{}".format(url, version),
+      license='MIT')
