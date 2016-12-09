35f44b036e8ab54747aa8a0872b81465d15ad075
Matthew Hartley <mhartley@cantab.net>
2016-12-09 13:19:23 +0000

    Fix Dockerfile entrypoint and build singularity image

--- deploy_singularity_image.sh
+++ deploy_singularity_image.sh
@@ -11,3 +11,14 @@ cp dist/*.whl deploy/docker/dtool-0.2.0-py3-none-any.whl
 # Build docker image
 docker build -t jicscicomp/dtool deploy/docker/
 
+# Build singularity image
+docker run \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    -v ~/mnt/cluster_home/singularity/:/output \
+    --privileged \
+    -t \
+    --rm \
+    mcdocker2singularity \
+    jicscicomp/dtool \
+    arctool \
+
