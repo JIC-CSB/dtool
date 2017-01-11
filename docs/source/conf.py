c06aed065eabfe5a30ddd5973c98208ad45d19c3
Tjelvar Olsson <tjelvar.olsson@jic.ac.uk>
2017-01-11 11:53:53 +0000

    Fix readthedocs problem with missing package

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,8 +32,7 @@ import os
 # ones.
 extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.doctest',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.githubpages']
+    'sphinx.ext.viewcode',]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
