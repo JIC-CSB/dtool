fb2b6f1a0470e63ac6cdf978402533ef71d210b2
Tjelvar Olsson <tjelvar.olsson@jic.ac.uk>
2017-02-09 16:48:09 +0000

    Create reuseable tmp_dir_fixture

---
+++ tests/__init__.py
@@ -0,0 +1,16 @@
+"""Test fixtures and utilities."""
+
+import shutil
+import tempfile
+
+import pytest
+
+
+@pytest.fixture
+def tmp_dir_fixture(request):
+    d = tempfile.mkdtemp()
+
+    @request.addfinalizer
+    def teardown():
+        shutil.rmtree(d)
+    return d
